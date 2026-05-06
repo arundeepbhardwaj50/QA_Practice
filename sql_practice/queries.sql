6a512ccd0cfaaf0768c59ba6f6c45cbd5793475b
arundeepbhardwaj50@gmail.com <arundeep>
2026-05-07 02:21:05 +0530

    Added SQL practice queries

---
+++ sql_practice/queries.sql
@@ -0,0 +1,12 @@
+-- Create table
+CREATE TABLE Employees (
+    Id INT,
+    Name VARCHAR(50),
+    Salary INT
+);
+
+-- Insert data
+INSERT INTO Employees VALUES (1, 'Arun', 30000);
+
+-- Select data
+SELECT * FROM Employees;
